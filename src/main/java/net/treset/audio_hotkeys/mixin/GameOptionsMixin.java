d7263839c92112cfbc16d341f7c3ed0054e35037
Tre5et <hanneshekl@gmail.com>
2022-06-09 19:33:10 +0200

    Cleaned up 1.19 build

--- src/main/java/net/treset/audio_hotkeys/mixin/GameOptionsMixin.java
+++
@@ -1,9 +0,0 @@
-package net.treset.audio_hotkeys.mixin;
-
-import net.minecraft.client.option.GameOptions;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(GameOptions.class)
-public interface GameOptionsMixin {
-
-}
